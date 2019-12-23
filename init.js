89730c8fe0c5a73d4177f33a6c09d9cf34109657
tomas-hartman <tomas.hartman@hotmail.com>
2019-12-23 20:57:31 +0100

    feat: classified

--- init.js
+++ init.js
@@ -1,38 +1,56 @@
-(function (){
-    const alphabet = "abcdefghijklmnoprstuvwyxz".split("");
-    let string = "Ahoj Barboro, jak se mas?";
-    const outputBase = 5; // how big segments should be the output formatted into
+class DecypherMe {
+    constructor(obj){
+        this._cypheringTable = obj.cypheringTable || null;
+        this._stringToEncode = obj.toEncode || null;   // zakódovat -- mám vlastní tabulku, nebo vygenerovat novou tabulku
+        this._stringToDecode = obj.toDecode || null;   // odkódovat -- potřebuje tabulku
+        this._getTable = obj.returnTable; // když je cyphering table null; zakódovat i odkódovat
+        this.outputBase = parseInt(obj.outputBase) || 5;
+        this.alphabet = "abcdefghijklmnoprstuvwyxz".split("");
+    }
 
-    const createDecypheringTable = () => {
-        const workingTable = [];
-        for(let i=0;i<5;i++){
-            let array = [];
-            for(let y=0;y<5;y++){
-                let rand = Math.floor(Math.random()*alphabet.length);
-                let letter = alphabet.splice(rand,1).toString();
-                array.push(letter);
-            }
-            workingTable.push(array);
+    get stringToEncode() {
+        if(this._stringToEncode) return this.prepareInput(this._stringToEncode);
+    }
+    
+    get stringToDecode() {
+        return this._stringToDecode;
+    }
+
+    get cypheringTable() {
+        const table = this._cypheringTable;
+        const checkInsideArrays = array => {
+            return array.length === 5;
         }
 
-        return workingTable;
-    }
-    const table = createDecypheringTable();
+        if(!table){
+            return this.createCypheringTable(); 
+        }
 
-    /**
-     * @todo Doesn't work with digits and special chars like žščřě etc.
-     * @param {string} string 
-     */
-    const prepareStr = (string) => {
+        if(table && Array.isArray(table) && table.length === 5 && table.every(checkInsideArrays)){
+            return table;
+        }
+
+        throw "Inserted table is corrupt, check if the table is 5x5 and contains 25 characters."; 
+        
+    }
+
+    get getTable() {
+        if(this._getTable && typeof this._getTable === "boolean"){
+            return this._getTable;
+        }
+    }
+
+    prepareInput = (string) => {
         string = string.toLowerCase();
         string = string.replace(/\W/g, '');
 
         return string;
     }
 
-    const prepareOutput = (string) => {
+    prepareOutput = (string) => {
+        const outputBase = this.outputBase;
         let output = "";
-        let slicer = outputBase;
+        let slicer = this.outputBase;
         let strStart = 0;
 
         if(string.length % outputBase !== 0){
@@ -41,7 +59,7 @@
         } 
 
         for(let i=0;i<string.length/outputBase;i++){
-            remainingString = string.slice(slicer);
+            let remainingString = string.slice(slicer);
             output += string.slice(strStart, slicer);
             output += " ";
 
@@ -55,8 +73,25 @@
         return output;
     }
 
-    const encodeToNums = (stringToEncode, decypheringTable) => {
-        const string = prepareStr(stringToEncode); 
+    createCypheringTable = () => {
+        const workingTable = [];
+        const alphabet = this.alphabet;
+
+        for(let i=0;i<5;i++){
+            let array = [];
+            for(let y=0;y<5;y++){
+                let rand = Math.floor(Math.random()*alphabet.length);
+                let letter = alphabet.splice(rand,1).toString();
+                array.push(letter);
+            }
+            workingTable.push(array);
+        }
+
+        return workingTable;
+    }
+
+    encodeToNums = (stringToEncode, decypheringTable) => {
+        const string = this.prepareInput(stringToEncode); 
         let output = "";
         let workingString = string.split("");
 
@@ -72,9 +107,8 @@
 
         return output;
     }
-    // let encodedString = encodeToNums(string, table);
 
-    const encodeToLetters = (stringToEncode, decypheringTable) => {
+    encodeToLetters = (stringToEncode, decypheringTable) => {
         let output = "";
         const workingString = stringToEncode.split("");
         const iterationLength = workingString.length;
@@ -87,46 +121,86 @@
 
         return output;
     }
-    // let encodedStringTwice = encodeToLetters(encodedString, table);
 
     /**
      * @todo work on some formatting and render output at clusters of 5
      * @param {string} stringToEncode 
      * @param {array} decypheringTable 
      */
-    const encode = (stringToEncode, decypheringTable) => {
-        const first = encodeToNums(stringToEncode, decypheringTable);
+    encode = (stringToEncode, decypheringTable) => {
+        const first = this.encodeToNums(stringToEncode, decypheringTable);
         let workingSecond = (Math.floor(Math.random()*5)).toString();
             workingSecond += first;
             workingSecond += (Math.floor(Math.random()*5)).toString();
 
-        if(workingSecond.length % outputBase !== 0){
+        if(workingSecond.length % this.outputBase !== 0){
             // const iterations = 6-(workingSecond.length % 6);
             // Compensates number of letters, so they are dividable by 6 / outputBase 
-            const iterations = (outputBase -(workingSecond.length / 2) % outputBase) * 2;
+            const iterations = (this.outputBase -(workingSecond.length / 2) % this.outputBase) * 2;
             for(let i=0;i<iterations;i++){
                 workingSecond += (Math.floor(Math.random()*5)).toString();
             }
         }
 
-        let output = encodeToLetters(workingSecond, decypheringTable);
+        let output = this.encodeToLetters(workingSecond, decypheringTable);
 
-        output = prepareOutput(output);
+        output = this.prepareOutput(output);
 
         return output;
     }
-    let properlyEncoded = encode(string, table);
 
-    const decode = (stringToDecode, decypheringTable) => {
+    decode = (stringToDecode, decypheringTable) => {
         const string = stringToDecode.replace(/\s/g, '');
         string.toLowerCase();
-        const first = encodeToNums(string, decypheringTable);
+        const first = this.encodeToNums(string, decypheringTable);
         let workingSecond = first.slice(1,first.length-1);
-        const second = encodeToLetters(workingSecond, decypheringTable);
+        const second = this.encodeToLetters(workingSecond, decypheringTable);
 
         return second;
     }
-    let properlyDecoded = decode(properlyEncoded, table);
-    debugger;
 
-})();
+    /**
+     * Variables come in play here - I'll decide which process should be run here.
+     */
+    run = () => {
+        const output = {};
+        if(this.getTable){
+            output.table = this.cypheringTable;
+        }
+
+        if(this.stringToEncode && !this.stringToDecode){
+            output.encoded = this.encode(this.stringToEncode, this.cypheringTable);
+        }
+        
+        if(!this.stringToEncode && this.stringToDecode){
+            output.decoded = this.decode(this.stringToDecode, this.cypheringTable); 
+        }
+
+        if(this.stringToEncode && this.stringToDecode){
+            throw "Input either string to encode or to decode.";
+        }
+
+        return output;
+    }
+}
+
+const string = "At the time of the murder Qahtani was Crown Prince Mohammed bin Salman's senior adviser. His role included directing media operations and an online campaign against the government's critics, including Khashoggi, according to US officials.";
+const string2 = "APTNW PTLXV CRNWK XITVF DKRDP SGEKN FYHUC ITRMV KXMBK MXPFP RMDFM CPMNY PSWHD PAULY FNRLI YHXPR MFXTV PPPGY KPTRH LXPPB VCYFD PTWSM DPSAP XSHSR XKAKC DPGRC FBPRM TTNWF WCZFT MXPSN NFTRT LKTRM FXTPR HBMBK NWHGR BKKVH TPRHR YXNYC BRLPB FMTUX";
+
+const testTable = [
+    ["a","b","c","d","e"],
+    ["f","g","h","i","j"],
+    ["k","l","m","n","o"],
+    ["p","r","s","t","u"],
+    ["v","w","x","y","z"]
+]
+
+const obj = {
+    toDecode: string2,
+    // toEncode: string,
+    // returnTable: true,
+    cypheringTable: testTable
+}
+
+const a = new DecypherMe(obj);
+console.log(a.run());
