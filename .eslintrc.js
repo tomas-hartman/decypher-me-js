508fcd9c9ca0fb9f4220f584b5a7d9a0c7182d6f
Tomas Hartman <tomas.hartman@hotmail.com>
2024-01-02 22:00:24 +0100

    chore (lint): set up prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,21 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'node': true,
-    'es2021': true
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  'overrides': [
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
     'object-curly-spacing': ['error', 'always'],
     'array-bracket-spacing': ['error', 'never'],
-    'computed-property-spacing': ['error', 'always'],
-    'semi-spacing': ['error', { 'before': false, 'after': true }]
-  }
-};
+    'semi-spacing': ['error', { before: false, after: true }],
+  },
+}
